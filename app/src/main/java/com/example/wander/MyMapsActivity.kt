256f8f0c04c6473cbf5f8f0a3b0cb4a7667f0d89
miro <minev.miro@gmail.com>
2021-02-27 23:50:36 +0200

    Add markers on long click

--- app/src/main/java/com/example/wander/MyMapsActivity.kt
+++ app/src/main/java/com/example/wander/MyMapsActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MyMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -40,6 +41,7 @@ class MyMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val home = LatLng(-26.206184, 28.062155)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, 10f))
         map.addMarker(MarkerOptions().position(home))
+        setMapLongClick(map)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -65,5 +67,24 @@ class MyMapsActivity : AppCompatActivity(), OnMapReadyCallback {
             true
         }
         else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            // A Snippet is Additional text that's displayed below the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))
+                    .snippet(snippet)
+            )
+        }
     }
 }
